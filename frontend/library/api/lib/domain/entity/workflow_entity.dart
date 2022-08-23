95495f66c8a151759d934dc17d9731f634e8d5d7
dkakunsi <deddy.kakunsi@gmail.com>
2022-08-23 08:50:35 +0700

    remove frontend code.

--- frontend/library/api/lib/domain/entity/workflow_entity.dart
+++
@@ -1,5 +0,0 @@
-import 'package:api/domain/entity/entity.dart';
-
-class WorkflowEntity extends Entity {
-  String get id => getValue('id');
-}
--- frontend/library/widget/lib/src/component/DataPage.dart
+++
@@ -1,7 +0,0 @@
-library widget;
-
-import 'package:flutter/material.dart';
-
-abstract class SearchableWidget extends StatefulWidget {
-  void search(String value);
-}
