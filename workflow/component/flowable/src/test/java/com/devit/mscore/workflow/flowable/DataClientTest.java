f0f5e6be13f3c92ee63bec09575d6683f05a28e9
dkakunsi <deddy.kakunsi@gmail.com>
2022-08-23 22:47:35 +0700

    Fix workflow tests.

---
+++ workflow/component/flowable/src/test/java/com/devit/mscore/workflow/flowable/DataClientTest.java
@@ -0,0 +1,55 @@
+package com.devit.mscore.workflow.flowable;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import com.devit.mscore.ServiceRegistration;
+import com.devit.mscore.exception.ApplicationRuntimeException;
+import com.devit.mscore.exception.RegistryException;
+import com.devit.mscore.web.Client;
+
+import org.junit.Test;
+
+public class DataClientTest {
+  @Test
+  public void testCreate_NotSupportClone() throws CloneNotSupportedException {
+    var client = mock(Client.class);
+    var serviceRegistration = mock(ServiceRegistration.class);
+
+    doReturn(client).when(client).clone();
+    doThrow(CloneNotSupportedException.class).when(serviceRegistration).clone();
+
+    assertThrows(ApplicationRuntimeException.class, () -> new DataClient(client, serviceRegistration, "workflowDomain", "workflowTaskDomain"));
+  }
+
+  @Test
+  public void testGetClient_NotSupportClone() throws CloneNotSupportedException {
+    var client = mock(Client.class);
+    var clonedClient = mock(Client.class);
+    var serviceRegistration = mock(ServiceRegistration.class);
+
+    doReturn(serviceRegistration).when(serviceRegistration).clone();
+    doReturn(clonedClient).when(client).clone();
+    doThrow(CloneNotSupportedException.class).when(clonedClient).clone();
+
+    var dataClient = new DataClient(client, serviceRegistration, "workflowDomain", "workflowTaskDomain");
+    assertThrows(ApplicationRuntimeException.class, () -> dataClient.getClient());
+  }
+
+  @Test
+  public void testGetUri_ThrowRegistryException() throws CloneNotSupportedException, RegistryException {
+    var client = mock(Client.class);
+    var serviceRegistration = mock(ServiceRegistration.class);
+
+    doReturn(client).when(client).clone();
+    doReturn(serviceRegistration).when(serviceRegistration).clone();
+    doThrow(RegistryException.class).when(serviceRegistration).get(anyString());
+
+    var dataClient = new DataClient(client, serviceRegistration, "workflowDomain", "workflowTaskDomain");
+
+    assertThrows(ApplicationRuntimeException.class, () -> dataClient.getWorkflowUri());
+  }
+}
