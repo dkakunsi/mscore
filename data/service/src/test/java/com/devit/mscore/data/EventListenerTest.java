f7ae10133a03e461a1118435e20c0cec9a419d19
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2022-09-15 12:02:14 +0700

    Add event listener test.

--- data/service/src/test/java/com/devit/mscore/data/EventListenerTest.java
+++ data/service/src/test/java/com/devit/mscore/data/EventListenerTest.java
@@ -1,6 +1,11 @@
 package com.devit.mscore.data;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -8,6 +13,7 @@ import com.devit.mscore.Event;
 import com.devit.mscore.Service;
 import com.devit.mscore.Subscriber;
 import com.devit.mscore.exception.ApplicationException;
+import com.devit.mscore.exception.ApplicationRuntimeException;
 
 import java.util.Map;
 
@@ -57,5 +63,32 @@ public class EventListenerTest {
     verify(domainService).delete(argumentCaptor.capture());
     var argument = argumentCaptor.getValue();
     assertTrue("id".equals(argument));
+  }
+
+  @Test
+  public void testListening_OnError_ShouldLogError() throws ApplicationException {
+    var subscriber = mock(Subscriber.class);
+    var domainService = mock(Service.class);
+    doThrow(ApplicationException.class).when(domainService).delete(anyString());
+    var services = Map.of("domain", domainService);
+    var eventListener = EventListener.of(subscriber, services);
+
+    var message = new JSONObject();
+    message.put(Event.EVENT, "remove");
+    message.put(Event.DOMAIN, "domain");
+    var jsonData = new JSONObject("{\"id\":\"id\"}");
+    message.put(Event.DATA, jsonData);
+
+    eventListener.consume(message);
+    verify(domainService).delete(anyString());
+  }
+
+  @Test
+  public void testCreateListener_NoService_ShouldFailed() throws ApplicationException {
+    var subscriber = mock(Subscriber.class);
+    Map<String, Service> services = Map.of();
+
+    var ex = assertThrows(ApplicationRuntimeException.class, () -> EventListener.of(subscriber, services));
+    assertThat(ex.getMessage(), is("Empty services is passed to EventListener"));
   }
 }
