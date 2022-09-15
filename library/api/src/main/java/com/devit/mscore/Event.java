3c7247170a55a0712747e753892670e1307a9870
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2022-09-15 07:37:07 +0700

    Create event class.

---
+++ library/api/src/main/java/com/devit/mscore/Event.java
@@ -0,0 +1,74 @@
+package com.devit.mscore;
+
+import com.devit.mscore.exception.ApplicationRuntimeException;
+
+import org.json.JSONObject;
+
+public final class Event {
+
+  public static final String EVENT = "event";
+
+  public static final String DOMAIN = "domain";
+
+  public static final String DATA = "data";
+
+  public static enum Type {
+    CREATE("create"),
+    UPDATE("update"),
+    REMOVE("remove");
+
+    private String name;
+
+    private Type(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return this.name;
+    }
+  }
+
+  private Type type;
+
+  private String domain;
+
+  private JSONObject data;
+
+  private Event(Type type, String domain, JSONObject data) {
+    this.type = type;
+    this.domain = domain;
+    this.data = data;
+  }
+
+  public static Event of(Type type, String domain, JSONObject data) {
+    return new Event(type, domain, data);
+  }
+
+  public static Event of(JSONObject event) {
+    try {
+      var type = event.getString(EVENT);
+      var domain = event.getString(DOMAIN);
+      var data = event.getJSONObject(DATA);
+      return of(Type.valueOf(type.toUpperCase()), domain, data);
+    } catch (Exception ex) {
+      throw new ApplicationRuntimeException(ex);
+    }
+  }
+
+  public String getAction() {
+    return String.format("%s.%s", this.domain, this.type);
+  }
+
+  public JSONObject getData() {
+    return new JSONObject(this.data.toMap());
+  }
+
+  public JSONObject toJson() {
+    var event = new JSONObject();
+    event.put(EVENT, this.type.toString());
+    event.put(DOMAIN, this.domain);
+    event.put(DATA, this.data);
+    return event;
+  }
+}
---
+++ library/api/src/test/java/com/devit/mscore/EventTest.java
@@ -0,0 +1,39 @@
+package com.devit.mscore;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import com.devit.mscore.exception.ApplicationRuntimeException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class EventTest {
+  
+  @Test
+  public void testCreate_WithJsonParameter_LowerCase() {
+    var json = new JSONObject();
+    json.put(Event.EVENT, "create");
+    json.put(Event.DOMAIN, "domain");
+    var jsonData = new JSONObject("{\"id\":\"id\"}");
+    json.put(Event.DATA, jsonData);
+    var event = Event.of(json);
+    assertNotNull(event);
+    assertThat(event.getAction(), is("domain.create"));
+    assertTrue(event.getData().similar(jsonData));
+    assertTrue(event.toJson().similar(json));
+  }
+
+  @Test
+  public void testCreate_ElementNotAvailable_ShouldThrowException() {
+    var json = new JSONObject();
+    json.put(Event.EVENT, "create");
+    json.put(Event.DOMAIN, "domain");
+    var ex = assertThrows(ApplicationRuntimeException.class, () -> Event.of(json));
+    assertTrue(ex.getCause() instanceof JSONException);
+  }
+}
