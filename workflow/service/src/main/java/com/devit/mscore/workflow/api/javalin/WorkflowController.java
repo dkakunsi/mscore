e0fa17aedb92facf44dbddf6c3ea21a6afd34232
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2022-11-08 15:55:33 +0800

    Dont catch in controller

--- workflow/service/src/main/java/com/devit/mscore/workflow/api/javalin/WorkflowController.java
+++ workflow/service/src/main/java/com/devit/mscore/workflow/api/javalin/WorkflowController.java
@@ -3,9 +3,7 @@ package com.devit.mscore.workflow.api.javalin;
 import static com.devit.mscore.util.Utils.VARIABLE;
 
 import com.devit.mscore.Event;
-import com.devit.mscore.Logger;
 import com.devit.mscore.WorkflowService;
-import com.devit.mscore.logging.ApplicationLogger;
 import com.devit.mscore.web.javalin.JavalinController;
 
 import org.json.JSONObject;
@@ -13,8 +11,6 @@ import org.json.JSONObject;
 import io.javalin.http.Handler;
 
 public class WorkflowController extends JavalinController {
-
-  private static final Logger LOGGER = ApplicationLogger.getLogger(WorkflowController.class);
 
   // TODO: move to core library constant
   private static final String ACTION = "action";
@@ -36,21 +32,16 @@ public class WorkflowController extends JavalinController {
   @Override
   public Handler post() {
     return ctx -> {
-      try {
-        var payload = new JSONObject(ctx.body());
-        var event = Event.of(payload);
-        var entity = event.getData().getJSONObject(ENTITY);
-        var variables = event.getData().optJSONObject(VARIABLE);
-        // TODO: load from event
-        var action = ctx.header(ACTION);
-        var processInstance = this.workflowProcess.executeWorkflow(action, entity, variables.toMap());
-  
-        var result = new JSONObject().put("instanceId", processInstance.getId());
-        ctx.status(SUCCESS).contentType(CONTENT_TYPE).result(result.toString());  
-      } catch (Throwable ex) {
-        LOGGER.error(ex.getMessage(), ex);
-        throw ex;
-      }
+      var payload = new JSONObject(ctx.body());
+      var event = Event.of(payload);
+      var entity = event.getData().getJSONObject(ENTITY);
+      var variables = event.getData().optJSONObject(VARIABLE);
+      // TODO: load from event
+      var action = ctx.header(ACTION);
+      var processInstance = this.workflowProcess.executeWorkflow(action, entity, variables.toMap());
+
+      var result = new JSONObject().put("instanceId", processInstance.getId());
+      ctx.status(SUCCESS).contentType(CONTENT_TYPE).result(result.toString());  
     };
   }
 }
--- workflow/service/src/main/java/com/devit/mscore/workflow/api/javalin/WorkflowTaskController.java
+++ workflow/service/src/main/java/com/devit/mscore/workflow/api/javalin/WorkflowTaskController.java
@@ -3,9 +3,7 @@ package com.devit.mscore.workflow.api.javalin;
 import static com.devit.mscore.util.AttributeConstants.ID;
 
 import com.devit.mscore.Event;
-import com.devit.mscore.Logger;
 import com.devit.mscore.WorkflowService;
-import com.devit.mscore.logging.ApplicationLogger;
 import com.devit.mscore.web.javalin.JavalinController;
 
 import org.json.JSONObject;
@@ -13,8 +11,6 @@ import org.json.JSONObject;
 import io.javalin.http.Handler;
 
 public class WorkflowTaskController extends JavalinController{
-
-  private static final Logger LOGGER = ApplicationLogger.getLogger(WorkflowTaskController.class);
 
   private WorkflowService workflowProcess;
 
@@ -31,18 +27,13 @@ public class WorkflowTaskController extends JavalinController{
   @Override
   public Handler put() {
     return ctx -> {
-      try {
-        var taskId = ctx.pathParam(ID);
-        var payload = new JSONObject(ctx.body());
-        var event = Event.of(payload);
-        var responseVariable = event.getData().toMap();
-        workflowProcess.completeTask(taskId, responseVariable);
-  
-        ctx.status(SUCCESS);
-      } catch (Throwable ex) {
-        LOGGER.error(ex.getMessage(), ex);
-        throw ex;
-      }
+      var taskId = ctx.pathParam(ID);
+      var payload = new JSONObject(ctx.body());
+      var event = Event.of(payload);
+      var responseVariable = event.getData().toMap();
+      workflowProcess.completeTask(taskId, responseVariable);
+
+      ctx.status(SUCCESS);
     };
   }
 }
