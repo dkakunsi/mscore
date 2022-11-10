f655edf9e357f36fbb6db380c932fa552e8fec16
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2022-11-10 19:38:25 +0800

    Update project.create workflow

---
+++ solution/scripts/pull-services.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker pull dkakunsi/reference-data:latest
+docker pull dkakunsi/reference-workflow:latest
+docker pull dkakunsi/reference-gateway:latest
+docker pull dkakunsi/reference-history:latest
+docker pull dkakunsi/reference-notification:latest
+docker pull dkakunsi/reference-configuration:latest
