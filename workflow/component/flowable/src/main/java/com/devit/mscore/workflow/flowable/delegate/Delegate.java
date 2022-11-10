cf91a5c0595de1db56965853e09bc21542082f69
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2022-11-10 14:06:13 +0800

    Add do nothing delegate
    To be used in service task class, so the execution is delegated to listeners

---
+++ workflow/component/flowable/src/main/java/com/devit/mscore/workflow/flowable/delegate/Delegate.java
@@ -0,0 +1,11 @@
+package com.devit.mscore.workflow.flowable.delegate;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+public class Delegate implements JavaDelegate {
+
+  @Override
+  public void execute(DelegateExecution execution) {
+  }  
+}
